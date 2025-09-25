63eee1ccb4035a8993a108458715d400d1b75848
dollydasheep <MelPoPo21@gmail.com>
2025-09-25 22:02:26 +0800

    Home usage component carousel card with pagination and connected devices

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -7,11 +7,12 @@ import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
 import * as NavigationBar from 'expo-navigation-bar';
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Svg, { Path } from 'react-native-svg';
-import { Image, type ImageStyle, View } from 'react-native';
+import { Dimensions, Image, type ImageStyle, View } from 'react-native';
 import { green } from 'react-native-reanimated/lib/typescript/Colors';
 import HomeUsage from '@/components/ui/homeusage';
+import Carousel from "react-native-reanimated-carousel";
 
 const LOGO = {
   light: require('@/assets/images/react-native-reusables-light.png'),
@@ -42,13 +43,59 @@ const IMAGE_STYLE: ImageStyle = {
   width: 76,
 };
 
+const homeCards = [
+  {
+    type: "usage"
+  },
+  {
+    type: "current_devices"
+  }
+];
+
+const width = Dimensions.get("window").width;
+
 export default function HomeScreen() {
   const { colorScheme } = useColorScheme();
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <>
-      <View className="flex flex-1 items-center gap-8 p-6 bg-background">
-        <HomeUsage />
+      <View className="flex flex-1 items-center p-6 bg-background">
+        <View className='flex flex-row justify-end w-full mb-2'>
+          <View className='flex flex-row gap-2'>
+            <View className={`w-2 h-2 ${currentIndex === 0 ? "bg-white" : "bg-gray-500"} rounded-full`}></View>
+            <View className={`w-2 h-2 ${currentIndex === 1 ? "bg-white" : "bg-gray-500"} rounded-full`}></View>
+            <View className={`w-2 h-2 ${currentIndex === 2 ? "bg-white" : "bg-gray-500"} rounded-full`}></View>
+            <View className={`w-2 h-2 ${currentIndex === 3 ? "bg-white" : "bg-gray-500"} rounded-full`}></View>
+          </View>
+        </View>
+        <Carousel
+          loop={false}
+          width={width * 0.9}
+          height={180}
+          autoPlay={false}
+          data={homeCards}
+          scrollAnimationDuration={500}
+          snapEnabled
+          mode="parallax"
+          modeConfig={{
+            parallaxScrollingScale: 1,   // shrink side cards
+            parallaxScrollingOffset: -20,   // gap between cards
+          }}
+          onProgressChange={(_, absoluteProgress) => {
+            setCurrentIndex(Math.round(absoluteProgress));
+          }}
+          renderItem={({ item }) => {
+            switch(item.type){
+              case "usage":
+                return <HomeUsage tab='usage' title='Total Usage'/>
+              case "current_devices":
+                return <HomeUsage tab='currentDevices' title="Connected Devices" />
+              default:
+                return <Text>Hello</Text>;
+            }
+          }}
+        />
       </View>
     </>
   );
--- components/ui/homeusage.tsx
+++ components/ui/homeusage.tsx
@@ -2,26 +2,93 @@ import { View } from "react-native";
 import { Text } from '@/components/ui/text';
 import { TrendingUp } from "lucide-react-native";
 
+type HomeUsageProps = {
+  tab: string
+  title: string
+}
 
-export default function HomeUsage(){
+export default function HomeUsage({tab, title} : HomeUsageProps){
   return(
     <>
       <View className='w-full p-4 bg-secondary rounded-lg'>
-        <View className='flex flex-row items-center gap-2 mb-4'>
-          <View className='w-2 h-2 rounded-full bg-green-400'></View>
-          <Text className='text-lg font-medium'>Total Usage</Text>
-        </View>
-        <Text className='text-4xl font-bold mb-1'>10.5 kW</Text>
-        <View className='flex flex-row justify-between'>
-          <View className='flex flex-row gap-1 items-center'>
-            <TrendingUp color="#22c55e" size={20} />
-            <Text className='text-green-400 text-sm'>Optimized Efficiency</Text>
-          </View>
-          <View className='flex flex-row gap-2 items-center'>
-            <View className='w-2 h-2 rounded-full bg-green-400'></View>
-            <Text className='text-green-400 text-sm'>0 Anomalies</Text>
-          </View>
-        </View>
+
+        {tab === "usage" && (
+          <>
+            <View className='flex flex-row items-center gap-2 mb-4'>
+              <View className='w-2 h-2 rounded-full bg-green-400'></View>
+              <Text className='text-lg font-medium'>{title}</Text>
+            </View>
+            <Text className='text-4xl font-bold mb-1'>10.5 kW</Text>
+            <View className='flex flex-row justify-between'>
+              <View className='flex flex-row gap-1 items-center'>
+                <TrendingUp color="#22c55e" size={20} />
+                <Text className='text-green-400 text-sm'>Optimized Efficiency</Text>
+              </View>
+              <View className='flex flex-row gap-2 items-center'>
+                <View className='w-2 h-2 rounded-full bg-green-400'></View>
+                <Text className='text-green-400 text-sm'>0 Anomalies</Text>
+              </View>
+            </View>
+          </>
+        )}
+
+        {tab === "currentDevices" && (
+          <>
+            <View className="w-full flex flex-row justify-between items-center mb-2">
+              <View className='flex flex-row items-center gap-2'>
+                <View className='w-2 h-2 rounded-full bg-green-400'></View>
+                <Text className='text-lg font-medium'>{title}</Text>
+              </View>
+              <View className='flex flex-row items-center gap-2'>
+                <View className='w-2 h-2 rounded-full bg-green-400'></View>
+                <Text className='text-xs text-green-400'>4 devices</Text>
+              </View>
+            </View>
+            <View className="flex flex-col gap-1">
+              <View className="flex flex-row items-center justify-between w-full">
+                <View className="flex flex-1 flex-row gap-2 items-center">
+                  <View className="h-2 w-2 rounded-full bg-red-500"></View>
+                  <Text className="text-sm truncate">Refrigerator</Text>
+                </View>
+                <View className="flex flex-row items-center gap-2">
+                  <TrendingUp color={"#22c55e"} size={20}/>      
+                  <View className="flex flex-row items-center gap-1">
+                    <Text className="text-sm w-10">(8%)</Text>
+                    <Text className="text-xs text-gray-400 w-12">4.5 kW</Text>
+                  </View>
+                </View>
+              </View>
+              
+              <View className="flex flex-row items-center justify-between w-full">
+                <View className="flex flex-1 flex-row gap-2 items-center">
+                  <View className="h-2 w-2 rounded-full bg-red-500"></View>
+                  <Text className="text-sm truncate">Oven</Text>
+                </View>
+                <View className="flex flex-row items-center gap-2">
+                  <TrendingUp color={"#22c55e"} size={20}/>      
+                  <View className="flex flex-row items-center gap-1">
+                    <Text className="text-sm w-10">(81%)</Text>
+                    <Text className="text-xs text-gray-400 w-12">84.5 kW</Text>
+                  </View>
+                </View>
+              </View>
+
+              <View className="flex flex-row items-center justify-between w-full">
+                <View className="flex flex-1 flex-row gap-2 items-center">
+                  <View className="h-2 w-2 rounded-full bg-red-500"></View>
+                  <Text className="text-sm truncate">Fan</Text>
+                </View>
+                <View className="flex flex-row items-center gap-2">
+                  <TrendingUp color={"#22c55e"} size={20}/>      
+                  <View className="flex flex-row items-center gap-1">
+                    <Text className="text-sm w-10">(58%)</Text>
+                    <Text className="text-xs text-gray-400 w-12">42.5 kW</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </>
+        )}
       </View>
     </>
   )
